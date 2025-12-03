82599ef1efc01384510833e2bca15ea2555f6974
riccardo-ubuntuv2 <riccardo@ubuntuv2.com>
2025-12-03 12:48:16 +0100

    Refactor obj_PairRDD to include ReduceByKey and GroupByKey operations with updated input file path

--- src/main/scala/sparkPratica/obj_PairRDD.scala
+++ src/main/scala/sparkPratica/obj_PairRDD.scala
@@ -1,5 +1,3 @@
-
-
 // Definisce il package in cui si trova questo oggetto
 package sparkPratica
 
@@ -10,6 +8,12 @@ import org.apache.spark.SparkContext
 
 // Oggetto principale che contiene il metodo main
 object obj_PairRDD {
+      // ReduceByKey: somma le quantità per ogni prodotto
+      println("**********ReduceByKey**********")
+      val red_by = pairRDD.reduceByKey(_ + _).collect()
+      for (i <- red_by) {
+        println(i)
+      }
   // Metodo main, punto di ingresso del programma
   def main(arg: Array[String]): Unit = {
     // Crea una configurazione Spark con nome applicazione e master locale
@@ -18,15 +22,24 @@ object obj_PairRDD {
     val sc = new SparkContext(conf)
     // Imposta il livello di log su "Error" per ridurre l'output
     sc.setLogLevel("Error")
-
     // Carica il file di testo come RDD da un percorso specificato
-    val inputRDD = sc.textFile("file:///C:/data/sales.txt")
-    // Crea una Pair RDD (chiave, valore) dove chiave è il primo campo e valore il secondo
-    val pairRDD = inputRDD.map(x => (x.split(" ")(0), x.split(" ")(1)))
+    val inputRDD = sc.textFile("/home/riccardo/Documenti/spark/sales.txt")
+    // Crea una Pair RDD (chiave, valore) dove la chiave è il primo campo e valore il secondo
+    val pairRDD = inputRDD.map { x =>
+      val arr = x.split(" ")
+      (arr(1), arr(2).toInt)
+    }
     // Colleziona gli elementi della Pair RDD in un array locale
-    val co=pairRDD.collect()
+    val co = pairRDD.collect()
     // Stampa ogni elemento della collezione
     for (i <- co) {
+      println(i)
+    }
+
+    // Raggruppa per chiave e stampa il risultato
+    println("**********GroupByKey**********")
+    val gr = pairRDD.groupByKey().collect()
+    for (i <- gr) {
       println(i)
     }
   }
