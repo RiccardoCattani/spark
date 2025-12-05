650231f8931349e62f7bb90cf218e290c3782a3a
Riccardo-portatile <riccardo@portatilerorcky>
2025-12-05 14:42:46 +0100

    Add ReadJsonExample object for reading simple and complex JSON files

---
+++ src/main/scala/sparkPratica/obj_jsonMultipleLine.scala
@@ -0,0 +1,29 @@
+package sparkPratica
+
+import org.apache.spark.sql.SparkSession
+
+object ReadJsonExample {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("Read JSON Example")
+      .master("local[*]")
+      .getOrCreate()
+
+    // Lettura JSON semplice
+    println("Leggo il file JSON semplice:")
+    val simpleDf = spark.read.format("json").load("user.json")
+    simpleDf.printSchema()
+    simpleDf.show(false)
+
+    // Lettura JSON complesso
+    println("Leggo il file JSON complesso:")
+    val complexDf = spark.read
+      .format("json")
+      .option("multiLine", true)
+      .load("random_user.json")
+    complexDf.printSchema()
+    complexDf.show(false)
+
+    spark.stop()
+  }
+}
